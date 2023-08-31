1932d551ca2a085cd841ef589c39b4ac1e5a29b1
erlanbelekov <belekoverlanhehe@gmail.com>
2023-08-31 15:59:19 +0600

    first commit for @kurut/cli

---
+++ packages/cli/README.md
@@ -0,0 +1,11 @@
+# @kurut/cli is CLI interface for next-gen Kurut ORM
+
+Available commands:
+
+```
+npx @kurut/cli auto-gen
+npx @kurut/cli migrate new
+npx @kurut/cli auto-gen
+npx @kurut/cli migrate run
+npx @kurut/cli db push
+```
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,3 +1,39 @@
-const figlet = require('figlet');
+import figlet from 'figlet';
+import { Command } from 'commander';
 
 console.log(figlet.textSync('Kurut CLI'));
+
+const program = new Command();
+
+program
+  .version('1.0.0')
+  .description(
+    'Kurut CLI for database migrations, local database sync and introscpection'
+  );
+
+// Commands for @kurut/cli migrate
+// Run migrations with no downtime
+// Generate TS SQL files up / down for each migration
+const migrate = program.command('migrate').description('Migrations');
+
+// Fetch last migration and find diff between current state and last migration state
+//
+migrate
+  .command('new')
+  .description('Create a new migration')
+  .action(() => {
+    return;
+  });
+
+migrate
+  .command('run')
+  .description('Run migrations on the database')
+  .action(() => {
+    return;
+  });
+
+program
+  .command('auto-gen')
+  .description('Auto-generate Kurut TypeScript entities from database');
+
+program.parse();
