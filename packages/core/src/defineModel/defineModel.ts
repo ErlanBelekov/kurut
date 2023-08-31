8df2960d37e9416b5a2f1c91215beedb9660388c
erlanbelekov <belekoverlanhehe@gmail.com>
2023-08-31 22:33:32 +0600

    add defineModel

---
+++ packages/core/src/defineModel/defineModel.ts
@@ -0,0 +1,31 @@
+import { FieldDefinition, KurutModel } from 'types';
+import { getClient } from 'getClient';
+
+// pass table definition to defineModel, and it will return a type-safe TS table definition
+export function defineModel<TFields extends Record<string, FieldDefinition>>(
+  tableName: string,
+  fields: TFields
+): KurutModel<TFields> {
+  return {
+    async throwableFindFirst() {
+      const client = await getClient(process.env.DATABASE_URL);
+
+      console.log('client: \n', client);
+
+      const text = `SELECT * FROM ${tableName}`;
+
+      const res = await client.query(text);
+
+      console.log(res.rows[0]);
+
+      return null;
+    },
+    async safeFindFirst() {
+      return null;
+    },
+    getMetaData: () => ({
+      fields,
+      tableName,
+    }),
+  };
+}
