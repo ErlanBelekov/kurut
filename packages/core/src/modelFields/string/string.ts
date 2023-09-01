76852d571f02a6444a6509cc4f1a71f719fe7c31
erlanbelekov <belekoverlanhehe@gmail.com>
2023-09-01 16:27:04 +0600

    create string() field definition

---
+++ packages/core/src/modelFields/string/string.ts
@@ -0,0 +1,10 @@
+import { FieldDefinition, FieldType } from '../../types';
+
+export function string(): FieldDefinition {
+  return {
+    type: FieldType.String,
+    isPrimaryKey: false,
+    isForeignKey: false,
+    nullable: false,
+  };
+}
